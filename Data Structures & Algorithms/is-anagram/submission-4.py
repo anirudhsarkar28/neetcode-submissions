05ffc9734d8e6679426355a1d1bdc6ca3f12bf13
Anirudh Sarkar <82276184+anirudhsarkar28@users.noreply.github.com>
2026-08-19 22:25:21 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s1 = list(s)
+        t1 = list(t)
+        if sorted(s1) == sorted(t1):
+            return True
+        else:
+            return False
+        
